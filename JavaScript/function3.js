df5393b2aef9ec6aa80e44807007346702f65716
stevenpeng106 <65689174+stevenpeng106@users.noreply.github.com>
2021-01-24 21:09:46 -0500

    Create function3.js

---
+++ JavaScript/function3.js
@@ -0,0 +1,9 @@
+// define isSnakeEyes below:
+function isSnakeEyes(x, y) {
+    if (x === 1 && y === 1) {
+        console.log('Snake Eyes!')
+    }
+    else {
+        console.log('Not Snake Eyes!')
+    }
+}
