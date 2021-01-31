bdde3cb240bfef3eafdfa10c549b03236165647e
stevenpeng106 <65689174+stevenpeng106@users.noreply.github.com>
2021-01-31 16:34:05 -0500

    Create dom5.js

---
+++ JavaScript/dom5.js
@@ -0,0 +1,7 @@
+const container = document.querySelector('div[id="container"]');
+container.style.textAlign = 'center';
+
+const image = document.querySelector('div img');
+image.style.width = '150px';
+image.style.borderRadius = '50%';
+
