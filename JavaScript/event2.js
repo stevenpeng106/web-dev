9254032fac12317ae9e237100cadba70342db89e
stevenpeng106 <65689174+stevenpeng106@users.noreply.github.com>
2021-02-01 13:41:28 -0500

    Create event2.js

---
+++ JavaScript/event2.js
@@ -0,0 +1,9 @@
+const b1 = document.querySelector('#hello');
+b1.addEventListener('click', function () {
+    console.log('hello');
+});
+
+const b2 = document.querySelector('#goodbye');
+b2.addEventListener('click', function () {
+    console.log('goodbye');
+});
